5b54b56a054690ddcc1810cbec5ba511dbb2e984
`thais` <`tjlannes@gmail.com`>
2023-02-01 19:13:01 +0100

    fix name button in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Component from "./components/AppComponent/AppComponent.js";
 
 import Button from "./components/ButtonComponent/ButtonComponent.js";
 
-const buttonTest = new Button("Enviar", document.body);
+const sendButton = new Button("Enviar", document.body);
 
-buttonTest.render();
-buttonTest.
+sendButton.render();

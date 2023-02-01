3821dea9e9fe1fce28465c1237702199c1f61749
`thais` <`tjlannes@gmail.com`>
2023-02-01 20:29:10 +0100

    add changes in the buttom

--- src/components/ButtonComponent/ButtonComponent.ts
+++ src/components/ButtonComponent/ButtonComponent.ts
@@ -1,7 +1,8 @@
 import Component from "../AppComponent/AppComponent.js";
 
-export default class Button extends Component {
+/*export default class Button extends Component {
   #text: string;
+  properties: HTMLButtonElement;
 
   constructor(
     text: string,
@@ -13,6 +14,28 @@ export default class Button extends Component {
   }
   render() {
     super.render();
+    this.element.textContent = this.#text;
+    this.element.
+  }
+}*/
+
+export default class Button {
+  element = document.createElement("button");
+  #parentElement: HTMLElement | null;
+  #text: string;
+
+  constructor(
+    text: string,
+    parentElement: HTMLElement | null,
+    cssClasses: string = ""
+  ) {
+    this.element.className = cssClasses;
+    this.#parentElement = parentElement;
+    this.#text = text;
+  }
+
+  render() {
+    this.#parentElement?.appendChild(this.element);
     this.element.textContent = this.#text;
   }
 }
